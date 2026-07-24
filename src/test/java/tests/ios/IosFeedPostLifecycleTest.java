ac5a8e9c240403b10943eb3c7c5a6b62bab7fb78
deno-fo <d4308970@gmail.com>
2026-07-24 18:52:42 +0300

    Implement complete iOS feed post lifecycle test

--- src/test/java/tests/ios/IosFeedPostLifecycleTest.java
+++ src/test/java/tests/ios/IosFeedPostLifecycleTest.java
@@ -1,20 +1,11 @@
 package tests.ios;
 
-import org.junit.jupiter.api.Test;
+import flows.ios.IosAccountDeletionFlow;
 import flows.ios.IosFeedPostFlow;
-
-public class IosFeedPostLifecycleTest extends BaseIosTest {
-
-    @Test
-    public void createLikeCommentDeletePost() {
-        String postText = "iOS feed lifecycle test";
-
-        IosFeedPostFlow flow =
-                new IosFeedPostFlow(driver);
-
-        flow.createTextPost(postText);
-        flow.likeTopPost();
-        flow.openComments();
-        flow.deletePost();
-    }
-}
+import flows.ios.IosPostLoginFlow;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.ios.EmailRegistrationPage;
+import pages.ios.FeedPage;
+import pages.ios.Login
