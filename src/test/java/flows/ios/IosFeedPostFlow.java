9b42c306f6d9a2d59f15f2f05cc723d664559c26
deno-fo <d4308970@gmail.com>
2026-07-25 00:35:40 +0300

    Add detailed feed flow timing logs

--- src/test/java/flows/ios/IosFeedPostFlow.java
+++ src/test/java/flows/ios/IosFeedPostFlow.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriverException;
 import pages.ios.FeedPage;
 import pages.ios.NewPostPage;
 import pages.ios.PostDetailsPage;
+import utils.StepTimer;
 
 import java.time.Duration;
 import java.util.Map;
@@ -40,12 +41,35 @@ public class IosFeedPostFlow {
     public void createTextPost(
             String postText
     ) {
-        feedPage.tapFeedTab();
-        dismissPossibleSystemNotificationBanner();
-        feedPage.waitUntilReady();
-        feedPage.openCreatePost();
-        newPostPage.publishTextPost(postText);
-        feedPage.waitUntilPostReady(postText);
+        StepTimer.run(
+                "Create post | Tap Feed tab",
+                feedPage::tapFeedTab
+        );
+
+        StepTimer.run(
+                "Create post | Dismiss notification banner",
+                this::dismissPossibleSystemNotificationBanner
+        );
+
+        StepTimer.run(
+                "Create post | Wait Feed ready",
+                feedPage::waitUntilReady
+        );
+
+        StepTimer.run(
+                "Create post | Open create form",
+                feedPage::openCreatePost
+        );
+
+        StepTimer.run(
+                "Create post | Fill and submit form",
+                () -> newPostPage.publishTextPost(postText)
+        );
+
+        StepTimer.run(
+                "Create post | Wait created post ready",
+                () -> feedPage.waitUntilPostReady(postText)
+        );
     }
 
     public void likePost(
@@ -63,14 +87,23 @@ public class IosFeedPostFlow {
     public void openPostComments(
             String postText
     ) {
-        feedPage.openPostComments(postText);
+        StepTimer.run(
+                "Comments | Tap comments action",
+                () -> feedPage.openPostComments(postText)
+        );
 
-        tutorialOverlay
-                .waitAndDismissIfPresent(
-                        Duration.ofMillis(300)
-                );
+        StepTimer.run(
+                "Comments | Dismiss tutorial overlay",
+                () -> tutorialOverlay
+                        .waitAndDismissIfPresent(
+                                Duration.ofMillis(300)
+                        )
+        );
 
-        postDetailsPage.waitUntilOpened();
+        StepTimer.run(
+                "Comments | Wait post details opened",
+                postDetailsPage::waitUntilOpened
+        );
     }
 
     public void addComment(
@@ -88,9 +121,22 @@ public class IosFeedPostFlow {
     public void deletePost(
             String postText
     ) {
-        postDetailsPage.deletePost();
-        feedPage.waitUntilReady();
-        feedPage.waitUntilPostDisappears(postText);
+        StepTimer.run(
+                "Delete post | Open menu and confirm",
+                postDetailsPage::deletePost
+        );
+
+        StepTimer.run(
+                "Delete post | Wait Feed ready",
+                feedPage::waitUntilReady
+        );
+
+        StepTimer.run(
+                "Delete post | Wait post disappeared",
+                () -> feedPage.waitUntilPostDisappears(
+                        postText
+                )
+        );
     }
 
     private void dismissPossibleSystemNotificationBanner() {
