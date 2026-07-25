5fbeb96fa5de225527714c0e00957507ff4cd456
deno-fo <d4308970@gmail.com>
2026-07-25 23:36:20 +0300

    Add iOS device test annotation

---
+++ src/test/java/annotations/IosDeviceTest.java
@@ -0,0 +1,22 @@
+package annotations;
+
+import extensions.IosDeviceTestExtension;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@TestTemplate
+@ExtendWith(IosDeviceTestExtension.class)
+@Execution(ExecutionMode.CONCURRENT)
+public @interface IosDeviceTest {
+}
