0e9d2c523ef9a05465087ea6f6d8239bc856bbc8
deno-fo <d4308970@gmail.com>
2026-07-25 21:41:25 +0300

    Refactor pre-made workout onboarding

--- src/test/java/tests/android/AndroidPreMadeWorkoutSelectionTest.java
+++ src/test/java/tests/android/AndroidPreMadeWorkoutSelectionTest.java
@@ -1,23 +1,27 @@
 package tests.android;
 
 import annotations.AndroidDeviceTest;
-import components.android.AndroidNotificationPermissionDialog;
 import components.android.TutorialOverlay;
 import flows.android.AccountDeletionFlow;
+import flows.android.AndroidOnboardingFlow;
 import flows.android.PreMadeWorkoutSelectionFlow;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import pages.android.*;
+import pages.android.EmailRegistrationPage;
+import pages.android.GoogleHealthPage;
+import pages.android.LoginPage;
+import pages.android.MainPage;
 import utils.TestData;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AndroidPreMadeWorkoutSelectionTest extends BaseAndroidTest {
+public class AndroidPreMadeWorkoutSelectionTest
+        extends BaseAndroidTest {
+
     private LoginPage loginPage;
     private EmailRegistrationPage emailRegistrationPage;
     private GoogleHealthPage googleHealthPage;
-    private BodyParametersPage bodyParametersPage;
-    private AndroidNotificationPermissionDialog notificationPermissionDialog;
+    private AndroidOnboardingFlow onboardingFlow;
     private PreMadeWorkoutSelectionFlow preMadeWorkoutSelectionFlow;
     private TutorialOverlay tutorialOverlay;
     private MainPage mainPage;
@@ -25,20 +29,33 @@ public class AndroidPreMadeWorkoutSelectionTest extends BaseAndroidTest {
 
     @BeforeEach
     public void createPages() {
-        loginPage = new LoginPage(driver);
-        emailRegistrationPage = new EmailRegistrationPage(driver);
-        googleHealthPage = new GoogleHealthPage(driver);
-        bodyParametersPage = new BodyParametersPage(driver);
-        notificationPermissionDialog = new AndroidNotificationPermissionDialog(driver);
-        preMadeWorkoutSelectionFlow = new PreMadeWorkoutSelectionFlow(driver);
-        tutorialOverlay = new TutorialOverlay(driver);
-        mainPage = new MainPage(driver);
-        accountDeletionFlow = new AccountDeletionFlow(driver);
+        loginPage =
+                new LoginPage(driver);
+
+        emailRegistrationPage =
+                new EmailRegistrationPage(driver);
+
+        googleHealthPage =
+                new GoogleHealthPage(driver);
+
+        onboardingFlow =
+                new AndroidOnboardingFlow(driver);
+
+        preMadeWorkoutSelectionFlow =
+                new PreMadeWorkoutSelectionFlow(driver);
+
+        tutorialOverlay =
+                new TutorialOverlay(driver);
+
+        mainPage =
+                new MainPage(driver);
+
+        accountDeletionFlow =
+                new AccountDeletionFlow(driver);
     }
 
     @AndroidDeviceTest
     public void shouldSelectPreMadeWorkoutForNewUser() {
-
         loginPage.skipWelcomeScreen();
         loginPage.openEmailAuthentication();
 
@@ -52,10 +69,16 @@ public class AndroidPreMadeWorkoutSelectionTest extends BaseAndroidTest {
         googleHealthPage.skipAccess();
         googleHealthPage.declinePersuasion();
         googleHealthPage.continueWithoutAccess();
-        bodyParametersPage.continueWithDefaultValues();
-        notificationPermissionDialog.allowNotifications();
-        preMadeWorkoutSelectionFlow.selectGeneralMuscleBuildingForGymAtExpertLevel();
+
+        onboardingFlow.completeRegistrationWithDefaultUserData(
+                preMadeWorkoutSelectionFlow::isReady
+        );
+
+        preMadeWorkoutSelectionFlow
+                .selectGeneralMuscleBuildingForGymAtExpertLevel();
+
         tutorialOverlay.dismiss();
+
         assertTrue(
                 mainPage.isPreMadeWorkoutSelected(
                         "General muscle building",
